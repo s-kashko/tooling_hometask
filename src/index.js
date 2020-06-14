280d75f5f85709a0fea5bcf8d7b127c81c5ba827
SerejKa-lab <seruogacom@mail.ru>
2020-06-15 00:47:04 +0300

    assets webpack config -> css, image, svg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Log from './log';
 import Message from './message';
 
+import flower from './assets/flower.jpg';
+import like from './assets/like.png';
+import neuro from './assets/neuro.webp';
+import telegram from './assets/telegram.svg';
+
+import './index.css';
+
+document.body.style.textAlign = 'center';
+
 const { log } = new Log();
 const { message } = new Message();
 
@@ -12,3 +21,30 @@ log(message('project!'));
 const hello = document.querySelector('.hello');
 hello.style.backgroundColor = 'yellowgreen';
 hello.style.textAlign = 'center';
+
+const flowerImg = document.createElement('img');
+flowerImg.src = flower;
+flowerImg.style.width = '100px';
+
+const likeImg = document.createElement('img');
+likeImg.src = like;
+likeImg.style.width = '100px';
+
+const neuroImg = document.createElement('img');
+neuroImg.src = neuro;
+neuroImg.style.width = '100px';
+
+const imagesBox = document.createElement('div');
+imagesBox.className = 'imagesBox';
+
+imagesBox.append(flowerImg);
+imagesBox.append(likeImg);
+imagesBox.append(neuroImg);
+
+document.body.append(imagesBox);
+
+const telegramImg = document.createElement('img');
+telegramImg.style.width = '200px';
+telegramImg.src = telegram;
+
+document.body.append(telegramImg);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')
-    }
+   mode: 'development',
+   entry: './src/index.js',
+
+   output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build')
+   },
+
+   module: {
+      rules: [
+
+         // loading images
+         {
+            test: /\.(png|jpe?g|gif|webp|svg)$/i,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     name: '[name]-[hash:8].[ext]',
+                     outputPath: 'images',
+                  },
+               },
+            ],
+         },
+
+         // loading css
+         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+         }
+      ],
+   }
 }
