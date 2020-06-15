13597d0fe4af6299cfbe623071193fda1ad3381f
SerejKa-lab <seruogacom@mail.ru>
2020-06-15 03:07:34 +0300

    prod & dev mode configs; code optimizations; npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,102 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-   mode: 'development',
-   entry: './src/index.js',
+module.exports = (env = {}) => {
 
-   output: {
-      filename: '[name].[hash].js',
-      path: path.resolve(__dirname, 'build')
-   },
+   const { mode = 'development' } = env;
+   const isProd = mode === 'production';
+   const isDev = mode === 'development';
 
-   module: {
-      rules: [
+   const getStyleLoaders = () => {
+      return [
+         isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+         'css-loader'
+      ]
+   }
 
-         // loading images
-         {
-            test: /\.(png|jpe?g|gif|webp|svg)$/i,
-            use: [
-               {
-                  loader: 'file-loader',
-                  options: {
-                     name: '[name]-[hash:8].[ext]',
-                     outputPath: 'images',
-                  },
-               },
-            ],
-         },
+   const getPlugins = () => {
+      const plugins = [
+         new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            title: 'Webpack project',
+            buildTime: new Date().toString(),
+            minify: {
+               collapseWhitespace: isProd
+            }
+         }),
+         new CleanWebpackPlugin()
+      ];
 
-         // loading css
-         {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
+      if (isProd) {
+         plugins.push(new MiniCssExtractPlugin({
+            filename: 'main.[hash].css'
+         })
+         )
+      }
+
+      return plugins;
+   }
+
+   const optimization = () => {
+      const config = {
+         splitChunks: {
+            chunks: 'all'
          }
-      ],
-   },
+      }
 
+      if (isProd) {
+         config.minimizer = [
+            new OptimizeCssAssetPlugin(),
+            new TerserPlugin()
+         ]
+      }
+   }
 
-   plugins: [
-      new HtmlWebpackPlugin({
-         template: 'public/index.html',
-         title: 'Webpack project',
-         buildTime: new Date().toString()
-      }),
+   return {
+      mode: isProd ? 'production' : isDev && 'development',
+      entry: './src/index.js',
 
-      new CleanWebpackPlugin()
-   ],
+      output: {
+         filename: '[name].[hash].js',
+         path: path.resolve(__dirname, 'build')
+      },
 
-   devServer: {
-      open: true,
-      port: 3000,
-      hot: true
+      module: {
+         rules: [
+
+            // loading images
+            {
+               test: /\.(png|jpe?g|gif|webp|svg)$/i,
+               use: [
+                  {
+                     loader: 'file-loader',
+                     options: {
+                        name: '[name].[hash:8].[ext]',
+                        outputPath: 'images',
+                     },
+                  },
+               ],
+            },
+
+            // loading css
+            {
+               test: /\.css$/i,
+               use: getStyleLoaders()
+            }
+         ],
+      },
+
+      plugins: getPlugins(),
+      optimization: optimization(),
+
+      devServer: {
+         open: true,
+         port: 3000,
+         hot: true
+      }
    }
 }
